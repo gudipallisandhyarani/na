b07f2dab8aaea628f8e5390de12835d7ce482208
gudipallisandhyarani <gudipallisandhyarani239@gmail.com>
2023-11-16 15:38:20 +0000

    repos

---
+++ src/components/RepositoryItem/index.js
@@ -0,0 +1,42 @@
+// Write your code here
+const RepositoryItem = props => {
+  const {repositoryDetails} = props
+
+  const {name, imageUrl, starsCount, forksCount, issuesCount} =
+    repositoryDetails
+
+  return (
+    <li className="repository-item">
+      <img className="repository-image" src={imageUrl} alt={name} />
+      <h1 className="repository-name">{name}</h1>
+      <div className="stars-conatainer">
+        <img
+          className="stars-icon"
+          src="https://assets.ccbp.in/frontend/react-js/stars-count-img.png"
+          alt="stars"
+        />
+        <p className="stars-text">{starsCount} stars</p>
+      </div>
+
+      <div className="stars-conatainer">
+        <img
+          className="stars-icon"
+          src="https://assets.ccbp.in/frontend/react-js/forks-count-img.png"
+          alt="forks"
+        />
+        <p className="stars-text">{forksCount} forks</p>
+      </div>
+
+      <div className="stars-conatainer">
+        <img
+          className="stars-icon"
+          src="https://assets.ccbp.in/frontend/react-js/issues-count-img.png"
+          alt="open issues"
+        />
+        <p className="stars-text">{issuesCount} open issues</p>
+      </div>
+    </li>
+  )
+}
+
+export default RepositoryItem
